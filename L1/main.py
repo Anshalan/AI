cddac358378a3bc915c1d69d0f89a8d8dd0fbaf7
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2022-02-12 17:23:23 +0100

    L1 at its best

--- L1/main.py
+++ L1/main.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 import random
 
@@ -9,23 +10,18 @@ class City_node:
         self.y = y
         self.connected_nodes = []
         self.path = []
-        self.remaining_cost_estimation = 0
-        self.total_estimation = 0#cost + remainig_cost_estimation
+        self.total_estimation = 0
         self.cost = 0
 
 
 def calculate_distance(node1: City_node, node2: City_node):
     return math.sqrt(pow(node1.x - node2.x, 2) + pow(node1.y - node2.y, 2))
 
+
 def generate_graph(n: int):
     graph = []
     for i in range(0, n):
         graph.append(City_node("City_" + str(i + 1), random.randrange(0, 5), random.randrange(0, 5)))
-        # graph.append(City_node("City_"+ str(i+1), i,2*i))
-    for city in graph:
-        for node in graph:
-            if city is not node:
-                city.connected_nodes.append(node)
     return graph
 
 
@@ -36,206 +32,160 @@ def print_list(c_list: list):
             print("\t" + nb.name + " " + str(calculate_distance(city, nb)))
 
 
-#
-def bfs(root):
-    # nodes_to_visit = [root]
-    root_cpy = City_node(root.name,root.x,root.y)
-    root_cpy.connected_nodes = root.connected_nodes
+def bfs(root, graph):
+    root_cpy = City_node(root.name, root.x, root.y)
     nodes_to_visit = [root_cpy]
     best_path = []
     best_cost = -1
     while len(nodes_to_visit) > 0:
-        current_node = nodes_to_visit.pop(0)  # 0
-        # print(current_node.name)
+        current_node = nodes_to_visit.pop(0)
         current_node.path.append(current_node.name)
-        if len(current_node.path) == len(root.connected_nodes) + 1:
+        if len(current_node.path) == len(graph):
             current_node.path.append(root.name)
             current_node.cost += calculate_distance(current_node, root)
-            # nodes_to_visit.append(current_node)
-            # print("almost done")
-        if len(current_node.path) == len(root.connected_nodes) + 2:
-
-            # print("path cost " + str(current_node.cost))
+        if len(current_node.path) == len(graph) + 1:
             if best_cost < 0 or current_node.cost < best_cost:
-                # if current_node.cost < best_cost:
                 best_cost = current_node.cost
                 best_path = current_node.path
         else:
-            for child in current_node.connected_nodes:
-                if (child.name not in current_node.path):
+            for child in graph:
+                if child.name not in current_node.path:
                     new_node = City_node(child.name, child.x, child.y)
                     new_node.cost = current_node.cost + calculate_distance(current_node, new_node)
                     new_node.path = list(current_node.path)
-                    new_node.connected_nodes = list(child.connected_nodes)
                     nodes_to_visit.append(new_node)
-    print("################")
-    print("BFS")
-    print(best_cost)
-    print(best_path)
-    # return best_path
+    # print("################")
+    # print("BFS ")
+    # print(best_cost)
+    # print(best_path)
+    return [best_path, best_cost]
 
 
-def dfs(root):
-    # nodes_to_visit = [root]
+def dfs(root, graph):
     root_cpy = City_node(root.name, root.x, root.y)
-    root_cpy.connected_nodes = root.connected_nodes
     nodes_to_visit = [root_cpy]
     best_path = []
     best_cost = -1
     while len(nodes_to_visit) > 0:
         current_node = nodes_to_visit.pop(0)  # 0
-        # print(current_node.name)
-
         current_node.path.append(current_node.name)
-        if len(current_node.path) == len(root.connected_nodes) + 1:
+        if len(current_node.path) == len(graph):
             current_node.path.append(root.name)
             current_node.cost += calculate_distance(current_node, root)
-            # nodes_to_visit.append(current_node)
-            # print("almost done")
-        if len(current_node.path) == len(root.connected_nodes) + 2:
-
-            # print("path cost " + str(current_node.cost))
+        if len(current_node.path) == len(graph) + 1:
             if best_cost < 0 or current_node.cost < best_cost:
-                # if current_node.cost < best_cost:
                 best_cost = current_node.cost
                 best_path = current_node.path
-
-
-        # if len(current_node.path) == len(root.connected_nodes) + 1:
-        #     # print("path cost " + str(current_node.cost))
-        #     if best_cost < 0 or current_node.cost < best_cost:
-        #         # if current_node.cost < best_cost:
-        #         best_cost = current_node.cost
-        #         best_path = current_node.path
         else:
             temp_nodes_to_visit = []
-            for child in current_node.connected_nodes:
-                if (child.name not in current_node.path):
+            for child in graph:
+                if child.name not in current_node.path:
                     new_node = City_node(child.name, child.x, child.y)
                     new_node.cost = current_node.cost + calculate_distance(current_node, new_node)
                     new_node.path = list(current_node.path)
-                    new_node.connected_nodes = list(child.connected_nodes)
-
                     temp_nodes_to_visit.append(new_node)
-                    # nodes_to_visit.append(new_node)
             nodes_to_visit = temp_nodes_to_visit + nodes_to_visit
-    print("################")
-    print("DFS")
-    print(best_cost)
-    print(best_path)
-def zachlanny(root):
-    # nodes_to_visit = [root]
+    # print("################")
+    # print("DFS")
+    # print(best_cost)
+    # print(best_path)
+    return [best_path, best_cost]
+
+
+def greedy(root, graph):
     root_cpy = City_node(root.name, root.x, root.y)
-    root_cpy.connected_nodes = root.connected_nodes
     nodes_to_visit = [root_cpy]
     best_path = []
     best_cost = -1
     while len(nodes_to_visit) > 0:
         current_node = nodes_to_visit.pop(0)  # 0
-        # print(current_node.name)
-
         current_node.path.append(current_node.name)
-
-        if len(current_node.path) == len(root.connected_nodes) + 1:
+        if len(current_node.path) == len(graph):
             current_node.path.append(root.name)
             current_node.cost += calculate_distance(current_node, root)
-            # nodes_to_visit.append(current_node)
-            # print("almost done")
-        if len(current_node.path) == len(root.connected_nodes) + 2:
+        if len(current_node.path) == len(graph) + 1:
 
-            # print("path cost " + str(current_node.cost))
             if best_cost < 0 or current_node.cost < best_cost:
-                # if current_node.cost < best_cost:
                 best_cost = current_node.cost
                 best_path = current_node.path
-
-
-
-        # if len(current_node.path) == len(root.connected_nodes) + 1:
-        #     # print("path cost " + str(current_node.cost))
-        #     if best_cost < 0 or current_node.cost < best_cost:
-        #         # if current_node.cost < best_cost:
-        #         best_cost = current_node.cost
-        #         best_path = current_node.path
         else:
             temp_nodes_to_visit = []
-            for child in current_node.connected_nodes:
-                if (child.name not in current_node.path):
+            for child in graph:
+                if child.name not in current_node.path:
                     new_node = City_node(child.name, child.x, child.y)
                     new_node.cost = current_node.cost + calculate_distance(current_node, new_node)
                     new_node.path = list(current_node.path)
-                    new_node.connected_nodes = list(child.connected_nodes)
-
                     temp_nodes_to_visit.append(new_node)
-                    # nodes_to_visit.append(new_node)
-                    # print(new_node.name + "cost is " + str(new_node.cost))
             newlist = sorted(temp_nodes_to_visit, key=lambda x: x.cost, reverse=False)
-            # print_list(newlist)
             nodes_to_visit.append(newlist[0])
+    # print("################")
+    # print("GREEDY")
+    # print(best_cost)
+    # print(best_path)
+    return [best_path, best_cost]
 
 
-    print("################")
-    print("GREEDY")
-    print(best_cost)
-    print(best_path)
-    # return best_path
-
-
-
-def a_star(root):
-    # nodes_to_visit = [root]
+def a_star(root, graph):
     root_cpy = City_node(root.name, root.x, root.y)
-    root_cpy.connected_nodes = root.connected_nodes
     nodes_to_visit = [root_cpy]
     best_path = []
     best_cost = -1
     while len(nodes_to_visit) > 0:
-        # print("presort")
-        newlist = sorted(nodes_to_visit, key=lambda x: x.total_estimation, reverse=False)
-        # print("postsort")
-        nodes_to_visit = newlist
-        # sort_nodes()
+        nodes_to_visit.sort(key=lambda x: x.total_estimation)
         current_node = nodes_to_visit.pop(0)  # 0
-        # print(current_node.name)
         current_node.path.append(current_node.name)
-        if len(current_node.path) == len(root.connected_nodes) + 1:
-            # print("path cost " + str(current_node.cost))
-            if best_cost < 0 or current_node.cost < best_cost:
-                # if current_node.cost < best_cost:
+        if len(current_node.path) >= len(graph):
+            if len(current_node.path) == len(graph) + 1:
                 best_cost = current_node.cost
                 best_path = current_node.path
+                break
+            new_node = City_node(root.name, root.x, root.y)
+            new_node.cost = current_node.cost + calculate_distance(current_node, new_node)
+            new_node.path = list(current_node.path)
+            new_node.total_estimation = new_node.cost + calculate_distance(new_node, root)
+            nodes_to_visit.append(new_node)
         else:
-            for child in current_node.connected_nodes:
+            temp_nodes_to_visit = []
+            for child in graph:
                 if (child.name not in current_node.path):
                     new_node = City_node(child.name, child.x, child.y)
                     new_node.cost = current_node.cost + calculate_distance(current_node, new_node)
                     new_node.path = list(current_node.path)
-                    new_node.connected_nodes = list(child.connected_nodes)
-                    new_node.remaining_cost_estimation = calculate_distance(new_node,root)
-                    new_node.total_estimation = new_node.cost + new_node.remaining_cost_estimation
-                    nodes_to_visit.append(new_node)
-    print("################")
-    print("A_STAR")
-    print(best_cost)
-    print(best_path)
+                    new_node.total_estimation = new_node.cost + calculate_distance(new_node, root)
+                    temp_nodes_to_visit.append(new_node)
+            nodes_to_visit = temp_nodes_to_visit + nodes_to_visit
+    # print("################")
+    # print("A_STAR ")
+    # print(best_cost)
+    # print(best_path)
+    return [best_path, best_cost]
 
 
+for i in range(5, 12):
+    graph = generate_graph(i)
 
-new_graph = generate_graph(8)
-new_graph2 = new_graph
-g2 = [
-    City_node("City_1", 0, 0),
-    City_node("City_2", 1, 0),
-    City_node("City_3", 0, 2)
-]
-for city in g2:
-    for node in g2:
-        if city is not node:
-            city.connected_nodes.append(node)
+    start_time = datetime.datetime.now()
+    bfs_res = bfs(graph[0], graph)
+    end_time = datetime.datetime.now()
+    bfs_execution_time = (end_time - start_time).total_seconds() * 1000
 
-print_list(new_graph)
-bfs(new_graph[0])
-# print_list(new_graph)
-dfs(new_graph[0])
-zachlanny(new_graph[0])
-# a_star(new_graph[0])
+    start_time = datetime.datetime.now()
+    dfs_res = dfs(graph[0], graph)
+    end_time = datetime.datetime.now()
+    dfs_execution_time = (end_time - start_time).total_seconds() * 1000
+
+    start_time = datetime.datetime.now()
+    greedy_res = greedy(graph[0], graph)
+    end_time = datetime.datetime.now()
+    z_execution_time = (end_time - start_time).total_seconds() * 1000
+
+    start_time = datetime.datetime.now()
+    a_star_res = a_star(graph[0], graph)
+    end_time = datetime.datetime.now()
+    a_execution_time = (end_time - start_time).total_seconds() * 1000
+
+    print("for " + str(i) + " nodes:"
+          + "\n\tbfs time:    {0}ms".format(round(bfs_execution_time, 1)) + "\tcost:{0} path: {1}".format(round(bfs_res[1], 3), bfs_res[0])
+          + "\n\tdfs time:    {0}ms".format(round(dfs_execution_time, 1))+ "\tcost:{0} path: {1}".format(round(dfs_res[1], 3), dfs_res[0])
+          + "\n\tgreedy time: {0}ms".format(round(z_execution_time, 1))+ "\tcost:{0} path: {1}".format(round(greedy_res[1], 3), greedy_res[0])
+          + "\n\ta* time:     {0}ms".format(round(a_execution_time, 1))+ "\tcost:{0} path: {1}".format(round(a_star_res[1], 3), a_star_res[0]))
