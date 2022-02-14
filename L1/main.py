19e6da894562bab29c5948b025586e233f24180e
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2022-02-14 11:13:31 +0100

    L1 + L2 done

--- L1/main.py
+++ L1/main.py
@@ -8,7 +8,6 @@ class City_node:
         self.name = name
         self.x = x
         self.y = y
-        self.connected_nodes = []
         self.path = []
         self.total_estimation = 0
         self.cost = 0
@@ -18,21 +17,14 @@ def calculate_distance(node1: City_node, node2: City_node):
     return math.sqrt(pow(node1.x - node2.x, 2) + pow(node1.y - node2.y, 2))
 
 
-def generate_graph(n: int):
-    graph = []
+def generate_cities(n: int):
+    cities_list = []
     for i in range(0, n):
-        graph.append(City_node("City_" + str(i + 1), random.randrange(0, 5), random.randrange(0, 5)))
-    return graph
+        cities_list.append(City_node("City_" + str(i + 1), random.randrange(0, 11), random.randrange(0, 11)))
+    return cities_list
 
 
-def print_list(c_list: list):
-    for city in c_list:
-        print(city.name)
-        for nb in city.connected_nodes:
-            print("\t" + nb.name + " " + str(calculate_distance(city, nb)))
-
-
-def bfs(root, graph):
+def bfs(root: City_node, graph: list):
     root_cpy = City_node(root.name, root.x, root.y)
     nodes_to_visit = [root_cpy]
     best_path = []
@@ -54,14 +46,10 @@ def bfs(root, graph):
                     new_node.cost = current_node.cost + calculate_distance(current_node, new_node)
                     new_node.path = list(current_node.path)
                     nodes_to_visit.append(new_node)
-    # print("################")
-    # print("BFS ")
-    # print(best_cost)
-    # print(best_path)
     return [best_path, best_cost]
 
 
-def dfs(root, graph):
+def dfs(root: City_node, graph: list):
     root_cpy = City_node(root.name, root.x, root.y)
     nodes_to_visit = [root_cpy]
     best_path = []
@@ -85,14 +73,10 @@ def dfs(root, graph):
                     new_node.path = list(current_node.path)
                     temp_nodes_to_visit.append(new_node)
             nodes_to_visit = temp_nodes_to_visit + nodes_to_visit
-    # print("################")
-    # print("DFS")
-    # print(best_cost)
-    # print(best_path)
     return [best_path, best_cost]
 
 
-def greedy(root, graph):
+def greedy(root: City_node, graph: list):
     root_cpy = City_node(root.name, root.x, root.y)
     nodes_to_visit = [root_cpy]
     best_path = []
@@ -118,14 +102,10 @@ def greedy(root, graph):
                     temp_nodes_to_visit.append(new_node)
             newlist = sorted(temp_nodes_to_visit, key=lambda x: x.cost, reverse=False)
             nodes_to_visit.append(newlist[0])
-    # print("################")
-    # print("GREEDY")
-    # print(best_cost)
-    # print(best_path)
     return [best_path, best_cost]
 
 
-def a_star(root, graph):
+def a_star(root: City_node, graph: list):
     root_cpy = City_node(root.name, root.x, root.y)
     nodes_to_visit = [root_cpy]
     best_path = []
@@ -154,15 +134,11 @@ def a_star(root, graph):
                     new_node.total_estimation = new_node.cost + calculate_distance(new_node, root)
                     temp_nodes_to_visit.append(new_node)
             nodes_to_visit = temp_nodes_to_visit + nodes_to_visit
-    # print("################")
-    # print("A_STAR ")
-    # print(best_cost)
-    # print(best_path)
     return [best_path, best_cost]
 
 
-for i in range(1, 11):
-    graph = generate_graph(i)
+for i in range(5, 12):
+    graph = generate_cities(i)
 
     start_time = datetime.datetime.now()
     bfs_res = bfs(graph[0], graph)
@@ -185,9 +161,11 @@ for i in range(1, 11):
     a_execution_time = (end_time - start_time).total_seconds() * 1000
 
     print("for " + str(i) + " nodes:"
-          + "\n\tbfs time:    {0}ms".format(round(bfs_execution_time, 1)) + "\tcost:{0} path: {1}".format(round(bfs_res[1], 3), bfs_res[0])
-          + "\n\tdfs time:    {0}ms".format(round(dfs_execution_time, 1))+ "\tcost:{0} path: {1}".format(round(dfs_res[1], 3), dfs_res[0])
-          + "\n\tgreedy time: {0}ms".format(round(z_execution_time, 1))+ "\tcost:{0} path: {1}".format(round(greedy_res[1], 3), greedy_res[0])
-          + "\n\ta* time:     {0}ms".format(round(a_execution_time, 1))+ "\tcost:{0} path: {1}".format(round(a_star_res[1], 3), a_star_res[0]))
-
-
+          + "\n\tbfs time:    {0}ms".format(round(bfs_execution_time, 1)) + "\tcost:{0} path: {1}".format(
+        round(bfs_res[1], 3), bfs_res[0])
+          + "\n\tdfs time:    {0}ms".format(round(dfs_execution_time, 1)) + "\tcost:{0} path: {1}".format(
+        round(dfs_res[1], 3), dfs_res[0])
+          + "\n\tgreedy time: {0}ms".format(round(z_execution_time, 1)) + "\tcost:{0} path: {1}".format(
+        round(greedy_res[1], 3), greedy_res[0])
+          + "\n\ta* time:     {0}ms".format(round(a_execution_time, 1)) + "\tcost:{0} path: {1}".format(
+        round(a_star_res[1], 3), a_star_res[0]))
--- L2/main.py
+++ L2/main.py
@@ -1,27 +1,30 @@
 import random
 
-cross_probability = 0.7
-mutation_probability = 0.1
-
-crossings = 0
-mutations = 0
-
-base_set = []
-base_set_binary = []
-
-for x in range(0, 128):
-    # for x in range(0, 10):
-    base_set.append(x)
-
-for element in base_set:
-    temp = bin(element).replace("0b", "").zfill(8)
-    # temp =  bin(element).replace("0b", "").zfill(4)
-    base_set_binary.append(temp)
-
-random.shuffle(base_set_binary)
+def generate_full_population():
+    ret = []
+    for x in range(0, 128):
+        ret.append(x)
+    return ret
 
 
-# def increase_crossings()
+def generate_random_population():
+    ret = []
+    for x in range(0, 128):
+        ret.append(random.randint(0, 127))
+    return ret
+
+
+def cast_to_bin(population):
+    ret = []
+    for element in population:
+        ret.append(bin(element).replace("0b", "").zfill(8))
+    return ret
+
+def cast_to_dec(population):
+    ret = []
+    for each in population:
+        ret.append(int(each, 2))
+    return ret
 
 def cross(population):
     for i in range(0, int(len(population) / 2)):
@@ -30,8 +33,8 @@ def cross(population):
             crossings += 2
             parent1 = population[2 * i]
             parent2 = population[2 * i + 1]
-            child1 = parent1[:int(len(parent1) / 2)] + parent2[int(len(parent1) / 2):]
-            child2 = parent1[:int(len(parent2) / 2)] + parent1[int(len(parent1) / 2):]
+            child1 = parent1[:int(len(parent1)/2)] + parent2[int(len(parent2)/2):]
+            child2 = parent2[:int(len(parent2)/2)] + parent1[int(len(parent1)/2):]
             population[2 * i] = child1
             population[2 * i + 1] = child2
 
@@ -55,28 +58,36 @@ def swap_value(thing, sign):
         thing = thing[:sign] + "0" + thing[sign + 1:]
     return thing
 
+
 def mutate(population):
     for each in population:
         if (random.random() < mutation_probability):
             global mutations
             mutations += 1
-            each = swap_value(each, random.randint(0, len(each)-1))
+            each = swap_value(each, random.randint(1, len(each) - 1))
 
-print(base_set_binary)
-for i in range(0, 51):
+
+
+cross_probability = 0.95
+mutation_probability = 0.01
+crossings = 0
+mutations = 0
+number_of_generations = 1000
+
+base_set = generate_full_population()
+# base_set = generate_random_population()
+base_set_binary = cast_to_bin(base_set)
+
+for i in range(0,number_of_generations):
+    if i > 0:
+        base_set_binary = get_new_generation(base_set_binary)
     random.shuffle(base_set_binary)
     mutate(base_set_binary)
     cross(base_set_binary)
-    base_set_binary.sort()
-    print("generation " + str(i) + str(base_set_binary))
-    base_set_binary = get_new_generation(base_set_binary)
-
+    # print("generation " + str(i+1) + str(base_set_binary))
+base_set_binary.sort(reverse=True)
+print("Populacja wejściowa\n" + str(base_set))
+print("Populacja po {0} generacjach\n".format(number_of_generations) + str(cast_to_dec(base_set_binary)))
 print("mutations: " + str(mutations))
 print("crossings: " + str(crossings))
 
-
-# aaaa= '10101010'
-# b = swap_value(aaaa, 4)
-# print(aaaa)
-# print(b)
-# get_new_generation(base_set_binary)
